ecffe7d9af68e86e70cae245ff8c53b2ae72693b
Chris Blumentritt <cblument@gmail.com>
2018-08-22 17:22:11 -0500

    Make script work with kitchen-ansible provisioner

--- install-omnibus-ansible-venv.sh
+++ install-omnibus-ansible-venv.sh
@@ -6,16 +6,27 @@ usage() {
   echo "How to use it"
 }
 
-if [ "$#" -gt 1 ]; then
+if [ "$#" -gt 2 ]; then
   usage
   exit 1
-elif [ "$1" == "-h" ]; then
-  usage
-  exit 0
-else
-  # set the version so pip can install
-  VERSION="$1"
 fi
+
+case "$1" in
+  "")
+    # Pass through will install latest version of ansible
+    ;;
+  "-h")
+    usage
+    exit 0
+    ;;
+  "-v")
+    shift
+    ANSIBLE_VERSION="$1"
+    ;;
+  *)
+    usage
+    exit 1
+esac
 
 apt-get update -q
 DEBIAN_FRONTEND=noninteractive apt-get -y install ruby && rm -rf /home/vagrant/.cache
@@ -24,9 +35,8 @@ virtualenv /opt/ansible-omnibus
 # Install the latest version of pip
 /opt/ansible-omnibus/bin/pip install -U pip
 
-
-if [ -z $VERSION ]; then
+if [ -z $ANSIBLE_VERSION ]; then
   /opt/ansible-omnibus/bin/pip install ansible
 else
-  /opt/ansible-omnibus/bin/pip install ansible==${VERSION}
+  /opt/ansible-omnibus/bin/pip install ansible==${ANSIBLE_VERSION}
 fi
